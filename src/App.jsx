b850fea32167646189b4315e68a2b69f02094882
Waltsuuu <waltteriheino@gmail.com>
2024-10-23 10:41:05 +0300

    Ran prettier on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,66 @@
-import { useState, useEffect } from 'react'
-import Note from './compontents/Note'
-import PropTypes from 'prop-types'
-import noteService from './Service/notes'
-import Notification from './compontents/Notification'
-import Footer from './compontents/Footer'
+import { useState, useEffect } from "react";
+import Note from "./compontents/Note";
+import PropTypes from "prop-types";
+import noteService from "./Service/notes";
+import Notification from "./compontents/Notification";
+import Footer from "./compontents/Footer";
 
 // START SERVER: json-server --port 3001 --watch db.json
 
-
 const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-const [notes, setNotes] = useState([])
-const [newNote, setNewNote] = useState('')
-const [showAll, setShowAll] = useState(true)
-const [errorMessage, setErrorMessage] = useState(null)
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      console.log("promise fulfilled");
+      setNotes(initialNotes);
+    });
+  }, []);
 
-useEffect(() => {
-  noteService
-    .getAll()
-    .then(initialNotes => {
-      console.log('promise fulfilled')
-      setNotes(initialNotes)
-    })
-}, [])
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
 
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
 
-const addNote = (event) => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() < 0.5,
-  }
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
 
-  noteService
-    .create(noteObject)
-    .then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
-}
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((note) => note.id === id);
+    const changedNote = { ...note, important: !note.important };
 
-const handleNoteChange = (event) => {
-  setNewNote(event.target.value)
-}
-
-const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
-
-const toggleImportanceOf = id => {
-  const note = notes.find(note => note.id === id)
-  const changedNote = { ...note, important: !note.important}
-
-  noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
-      setNotes(notes.map(note => note.id !== id ? note :
-         returnedNote))
-  })
-  // eslint-disable-next-line no-unused-vars
-  .catch(error => {
-      setErrorMessage(
-        `Note '${note.content}' was already removed from server`
-      )
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
-}
-
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      // eslint-disable-next-line no-unused-vars
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
 
   return (
     <div>
@@ -77,30 +69,33 @@ const toggleImportanceOf = id => {
 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          Show {showAll ? 'important' : 'all' }
+          Show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
-      {notesToShow.map(note =>
-        <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
-        )}  
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input 
-          placeholder='Write a note...'
+        <input
+          placeholder="Write a note..."
           value={newNote}
           onChange={handleNoteChange}
         />
         <button type="submit">Save Note</button>
-      </form>   
+      </form>
       <Footer />
-
     </div>
-  )
-}
+  );
+};
 
 App.propTypes = {
-  notes: PropTypes.array
-}
+  notes: PropTypes.array,
+};
 
-export default App
+export default App;
